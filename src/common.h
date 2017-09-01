dd2b8aad1c48896490ac51194ced7db199c1f6c3
homugiken <homugiken@gmail.com>
2017-09-01 09:54:35 +0800

    On branch master
    Initialize the repo.
    
    Changes to be committed:
    	new file:   src/Makefile
    	new file:   src/common.h
    	new file:   src/main.c

---
+++ src/common.h
@@ -0,0 +1,19 @@
+/*==============================================================================
+ * common.h
+ -----------------------------------------------------------------------------*/
+#ifndef __COMMON_H__
+#define __COMMON_H__
+
+#include <stdio.h>
+#include <stdint.h>
+#include <stdbool.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <termios.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+
+
+#endif	/*__COMMON_H__*/
